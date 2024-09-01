8ef3a7fd5a5b780484e4b293c46d3bf1e19f5080
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-08-31 22:29:00 -0400

    Add anthropic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import Anthropic from "@anthropic-ai/sdk";
+
+const client = new Anthropic({
+  apiKey: process.env["ANTHROPIC_API_KEY"],
+});
+
+async function main() {
+  const message = await client.messages.create({
+    max_tokens: 1024,
+    messages: [{ role: "user", content: "Hello, Claude" }],
+    model: "claude-3-opus-20240229",
+  });
+
+  console.log(message.content);
+}
+
+main();
+
