4d5bc2e7a32416c12c1b6b5d5611ee6a131e7273
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-09-01 00:16:20 -0400

    add all functionality (minus AI)

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import Anthropic from "@anthropic-ai/sdk";
 import { Database } from "bun:sqlite";
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs/yargs";
 
 const client = new Anthropic({
   apiKey: process.env["ANTHROPIC_API_KEY"],
 });
 
-async function main() {
-  const db = new Database("./database.sqlite");
+/**
+ * Gets the messages for a recipient by name from the SQLite database.
+ * @param name Recipient's name in the database.
+ * @param fp The file path to the SQLite database.
+ * @returns The messages for the recipient.
+ */
+const getMessages = async (name: string, fp = "./output/database.sqlite") => {
+  const db = new Database(fp);
   const query =
-    db.query(`SELECT r2.profile_joined_name,message.body FROM recipient RIGHT JOIN thread RIGHT JOIN message RIGHT JOIN recipient AS r2 WHERE lower(recipient.profile_joined_name) REGEXP ".*wol.*" AND recipient._id==thread.recipient_id AND thread._id==message.thread_id AND message.from_recipient_id==r2._id AND message.body!="";
-`);
-  query.get();
-  
-  // const message = await client.messages.create({
-  //   max_tokens: 1024,
-  //   messages: [{ role: "user", content: "Hello, Claude" }],
-  //   model: "claude-3-opus-20240229",
-  // });
-  //
-  // console.log(message.content);
+    db.query(`SELECT r2.profile_joined_name,message.body FROM recipient RIGHT JOIN thread RIGHT JOIN message RIGHT JOIN recipient AS r2 WHERE lower(recipient.profile_joined_name) LIKE "%${name}%" AND recipient._id==thread.recipient_id AND thread._id==message.thread_id AND message.from_recipient_id==r2._id AND message.body!="";`);
+  return query.all();
 }
 
-main();
+const argv = yargs(hideBin(process.argv))
+  .command('list-messages [name]', "List messages by recipient name", (yargs) => {
+    return yargs.positional("name", {
+      description: "Recipient name to filter messages by",
+      type: "string",
+    })
+  }, async (argv) => {
+    const { name } = argv;
+    console.log(`Listing messages for recipient "${name}"`);
+    if (name === undefined) {
+      console.error("Recipient name is required");
+      process.exit(1);
+    }
+    (await getMessages(name)).forEach((el: any) => console.log(`${el.profile_joined_name}: ${el.body}`));
+  })
+  .parse();
