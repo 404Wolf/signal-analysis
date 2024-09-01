4564ea2e179e7fd4cc5ca47682c7c67c01874636
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-09-01 01:49:59 -0400

    Fancy high tech very important AI work

--- src/ai.ts
+++ src/ai.ts
@@ -15,7 +15,9 @@ const anthropicClient = new Anthropic({
  * @param {Message} message
  * @returns {Promise<boolean>}
  */
-export const isAProject = async function (message: Message): Promise<boolean> {
+export const isAProject = async function (
+  messages: Message[],
+): Promise<boolean> {
   const chatCompletion = await openAiClient.chat.completions.create({
     messages: [
       {
@@ -25,7 +27,7 @@ export const isAProject = async function (message: Message): Promise<boolean> {
       },
       {
         role: "user",
-        content: `Are we talking about a project? "YES" OR "NO" ${message.body}`,
+        content: `Are we talking about a project? "YES" OR "NO" ${messages.map((message) => message.body).join("\n")}`,
       },
     ],
     model: "gpt-4o-mini",
@@ -44,55 +46,43 @@ export const isAProject = async function (message: Message): Promise<boolean> {
  * @param {Message} message
  * @returns {Promise<string>}
  */
-export const tldrOfProject = async (message: Message): Promise<string> => {
+export const tldrOfProject = async (messages: Message[]): Promise<string> => {
   const chatCompletion = await anthropicClient.messages.create({
     max_tokens: 20,
     model: "claude-3-opus-20240229",
     messages: [
       {
         role: "user",
-        content: `TLDR for "we should make a home manager standalone utility"`,
+        content: `SHORT TLDR for "we should make a home manager standalone utility". ONLY REFERENCE THE PROJECT IDEA ITSELF. BE SUPER PITHY. TLDR. NO ADJECTIVES.`,
       },
       { role: "assistant", content: "Home manager standalone utility" },
-      { role: "user", content: `TLDR for ${message.body}` },
+      {
+        role: "user",
+        content: `TLDR for ${messages.map((message) => message.body).join("\n")}. NO ADJECTIVES!`,
+      },
     ],
   });
 
   return (chatCompletion.content[0] as any).text;
 };
 
-const possibleProjectId = {
-  name: "Wolf Mermelstein",
-  body: "we should make pypi OS",
-};
-
-const itIsAProject = await isAProject(possibleProjectId);
-
-if (itIsAProject) {
-  const tldr = await tldrOfProject(possibleProjectId);
-  console.log(tldr);
-}
-
 export const whoseProjectIdea = async (projectIdea: Message[]) => {
+  let formattedProjectConversation = projectIdea
+    .map(
+      (projectIdea) => `${projectIdea.name.toUpperCase()}: ${projectIdea.body}`,
+    )
+    .join("\n");
+
   const chatCompletion = await openAiClient.chat.completions.create({
     messages: [
       {
         role: "system",
         content:
-          'You figure out whose idea is whose. You are told\n"NAME=JOHN SMITH" and\n"PROJECT=WE SHOULD MAKE A HOME MANAGER STANDALONE UTILITY".\nYou ONLY SAY the NAME of the person whose idea it is.',
+          'You are a machine who determines whose idea a project is. You ONLY say the name of the person. So say like, "JOHN SMITH"',
       },
       {
         role: "user",
-        content:
-          "NAME='WOLF MERMELSTEIN';PROJECT='We should do hyprland but in rust",
-      },
-      {
-        role: "assistant",
-        content: "Wolf Mermelstein",
-      },
-      {
-        role: "user",
-        content: `Whose idea is this? NAME OR PROJECT\nNAME='${possibleProjectId.name.toUpperCase()}';PROJECT='${possibleProjectId.body}'`,
+        content: `Whose idea is this project? ${formattedProjectConversation}`,
       },
     ],
     model: "gpt-4o-mini",
@@ -100,4 +90,3 @@ export const whoseProjectIdea = async (projectIdea: Message[]) => {
 
   return chatCompletion.choices[0].message.content;
 };
-
